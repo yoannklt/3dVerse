b0fd318b38a16ed40d163b43d4c5994cdb530c48
Kerly <115028766+yoannklt@users.noreply.github.com>
2023-10-11 15:29:00 +0200

    nique ta mère

--- public/app.js
+++ public/app.js
@@ -1,27 +1,36 @@
-var AppConfig = require('./AppConfig.js');
-
 window.addEventListener('load', async () =>
-            {
-                const connectionInfo = await SDK3DVerse.webAPI.createOrJoinSession(AppConfig.sceneUUID);
-
-                SDK3DVerse.notifier.on('onLoadingStarted', () => document.getElementById("message").innerHTML = "Connecting...");
-                SDK3DVerse.notifier.on('onLoadingProgress', (status) => document.getElementById("message").innerHTML = status.message);
-                SDK3DVerse.notifier.on('onLoadingEnded', (status) => document.getElementById("message").innerHTML = status.message);
-
-                SDK3DVerse.setupDisplay(document.getElementById('display_canvas'));
-                SDK3DVerse.startStreamer(connectionInfo);
-
-            });
-
-const setEntityWithEUID = async (EUID, entity) =>
 {
-    SDK3DVerse.engineAPI.findEntitiesByEUID(EUID).then(entities =>
-        {  
-            entity = entities[0];
-        });
-};
- 
-setEntityWithEUID('f82224d3-0203-4638-b30c-eb052de8cb5a', cube);
+    const connectionInfo = await SDK3DVerse.webAPI.createOrJoinSession(AppConfig.sceneUUID);
 
-cube.setPosition([0, 0, 0]);
-SDK3DVerse.propagateChanges();
+    SDK3DVerse.notifier.on('onLoadingStarted', () => document.getElementById("message").innerHTML = "Connecting...");
+    SDK3DVerse.notifier.on('onLoadingProgress', (status) => document.getElementById("message").innerHTML = status.message);
+    SDK3DVerse.notifier.on('onLoadingEnded', (status) => document.getElementById("message").innerHTML = status.message);
+
+    SDK3DVerse.setupDisplay(document.getElementById('display_canvas'));
+    SDK3DVerse.startStreamer(connectionInfo);
+
+});
+
+
+async function setEntitiesWithEUID(EUID)
+{ 
+    return  SDK3DVerse.engineAPI.findEntitiesByEUID(EUID)[0]
+}
+
+var cube = await setEntitiesWithEUID('f82224d3-0203-4638-b30c-eb052de8cb5a');
+
+cube.setPosition([1, 1, 1]);
+
+const commitChanges = () =>
+{
+    SDK3DVerse.engineAPI.commitChanges();
+}
+
+const propagateChanges = () =>
+{
+    SDK3DVerse.engineAPI.propagateChanges();
+}
+
+commitChanges();
+
+console.log("nique ta mère");
