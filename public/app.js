49cb974726ca776e86c460a2494961648d9aea84
Kerly <115028766+yoannklt@users.noreply.github.com>
2023-10-11 15:33:04 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -9,27 +9,6 @@ window.addEventListener('load', async () =>
     SDK3DVerse.setupDisplay(document.getElementById('display_canvas'));
     SDK3DVerse.startStreamer(connectionInfo);
 
-    async function setEntitiesWithEUID(EUID)
-    { 
-        return  SDK3DVerse.engineAPI.findEntitiesByEUID(EUID)[0]
-    }
-
-    var cube = setEntitiesWithEUID('f82224d3-0203-4638-b30c-eb052de8cb5a');
-
-    cube.setPosition([1, 1, 1]);
-
-    const commitChanges = () =>
-    {
-        SDK3DVerse.engineAPI.commitChanges();
-    }
-
-    const propagateChanges = () =>
-    {
-        SDK3DVerse.engineAPI.propagateChanges();
-    }
-
-    commitChanges();
-
     console.log("nique ta mère");
 
 });
